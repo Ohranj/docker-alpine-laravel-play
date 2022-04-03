d1886927dd33296e1018ecec0219e7f520cd5ae1
Ohranj <ajdorrington@hotmail.co.uk>
2022-04-03 19:51:43 +0100

    Resolved merge conflict

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,18 @@
 
 @section('main-content')
 <!-- prettier-ignore -->
-<div class="m-auto h-1/2 w-full px-1 border">
+<div class="m-auto h-1/2 w-full px-1">
+    <h1 class="text-center text-5xl mb-5 hover:text-red-500">Fitness Tracker</h1>
     {{-- <!-- <x-auth-validation-errors class="mb-4" :errors="$errors" /> --> --}}
     <form method="POST" action="{{ route('login') }}" x-data="loginForm({ csrfToken: '{{csrf_token()}}' })">
         {{-- @csrf --}}
         <div>
-            <label for="email">Email</label>
-            <input id="email" class="block mt-1 w-full" type="email" name="email" required autofocus placeholder="Enter your email..." />
+            <label x-ref="emailLabel" for="email">Email</label>
+            <input x-on:focus="$refs.emailLabel.classList.add('text-red-500')" x-on:focusout="$refs.emailLabel.classList.remove('text-red-500')" class="block mt-1 w-full" type="email" name="email" required placeholder="Enter your email..." />
         </div>
         <div class="mt-4">
-            <label for="password">Password</label>
-            <input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Enter your password..." />
+            <label x-ref="passwordLabel" for="password">Password</label>
+            <input x-on:focus="$refs.passwordLabel.classList.add('text-red-500')" x-on:focusout="$refs.passwordLabel.classList.remove('text-red-500')" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Enter your password..." />
         </div>
         <div class="block mt-4">
             <label for="remember_me" hidden>Remember me</label>
@@ -22,20 +23,17 @@
         </div>
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">Forgot your password</a>
-            <button class="app-btn-primary">Log in</button>
+            <button class="app-btn app-btn-primary">Register</button>
+            <button class="app-btn app-btn-secondary">Log in</button>
         </div>
     </form>
-
-    <div x-data="contactUsForm({ csrfToken: '{{csrf_token()}}' })">
-        <button @click="showModal = true" class="app-btn-primary">Contact Us</button>
-        <x-contact-us />
-    </div>
 </div>
 
 @endsection
 
 <!-- prettier-ignore -->
 @section('script')
+<script src="/js/contactUsForm.js"></script>
 <script defer>
     const loginForm = ({ csrfToken }) => ({
         csrfToken,
@@ -44,5 +42,4 @@
         },
     });
 </script>
-<script src="/js/contactUsForm.js"></script>
 @endsection
--- resources/views/components/contact-us.blade.php
+++ resources/views/components/contact-us.blade.php
@@ -1,11 +1,11 @@
 <!-- prettier-ignore -->
-<div x-cloak x-show="showModal" :class="showModal ? 'flex' : ''" class="m-auto items-center fixed inset-0 z-50 w-11/12 sm:w-[650px] md:w-[700px] h-full">
+<div x-cloak x-show="showModal" :class="showModal ? 'flex' : ''" class="m-auto items-center fixed inset-0 z-50 h-full app-sm-modal">
     <div class="p-4 w-full">
         <div class="bg-white rounded-lg shadow dark:bg-gray-700">
-            <div class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">Contact Us</h3>
-                <button @click="showModal = false" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <div class="flex justify-between items-start p-5 rounded-t border-b bg-slate-700">
+                <h3 class="text-xl font-semibold lg:text-2xl">Contact Us</h3>
+                <button @click="showModal = false" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg> 
                 </button>
@@ -18,10 +18,10 @@
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa est, vitae suscipit similique nam, voluptas illo minus reprehenderit accusamus ad amet repellat odit aut at optio voluptatum commodi quod facilis facere libero aspernatur deleniti recusandae! Rerum, pariatur iure sapiente asperiores labore odio odit aliquam ipsa, non nisi mollitia ex. Earum eaque nemo magnam expedita vel, non maxime nam voluptatem pariatur quisquam amet fugit corrupti provident inventore quae quam laudantium vitae? Tempora accusantium saepe adipisci ipsa.
                 </p>
             </div>
-            <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                <button type="button" class="app-btn-primary">I accept</button>
-                <button @click="showModal = false" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                    Decli
+            <div class="flex items-center bg-slate-700 p-6 space-x-2 rounded-b border-t border-gray-200">
+                <button type="button" class="app-btn app-btn-primary">Submit</button>
+                <button @click="showModal = false" type="button" class="app-btn app-btn-secondary">
+                    Cancel
                 </button>
             </div>
         </div>
