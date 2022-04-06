8de5ba4cd285681b4fc40a35cfc1174350415528
Ohranj <ajdorrington@hotmail.co.uk>
2022-04-06 23:14:29 +0100

    Added validation error from form submit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,22 @@
 
 @section('main-content')
 <!-- prettier-ignore -->
-<div class="m-auto h-1/2 w-full px-1">
-    <h1 class="text-center text-4xl sm:text-5xl mb-5 sm:mb-8 hover:text-red-500">Fitness Tracker</h1>
-    {{-- <!-- <x-auth-validation-errors class="mb-4" :errors="$errors" /> --> --}}
-    <form method="POST" action="{{ route('login') }}" x-data="loginForm({ csrfToken: '{{csrf_token()}}', loginURL: '{{ route('login.submit') }}' })" class="p-3 shadow-xl shadow-red-300 rounded" id="f_login">
+<div x-data="loginForm({ csrfToken: '{{csrf_token()}}', loginURL: '{{ route('login.submit') }}' })" class="m-auto h-1/2 w-full px-1">
+    <h1 class="text-center text-4xl sm:text-5xl mb-5 sm:mb-8" :class="submitSuccess ? 'text-red-500' : 'hover:text-red-500'">Fitness Tracker</h1>
+    <form x-show="!submitSuccess" method="POST" action="{{ route('login') }}" class="p-3 shadow-xl shadow-red-300 rounded" id="f_login">
         @csrf
+        <p x-cloak x-show="submitFailed" class="text-red-500 text-center py-4">Invalid credentials. Please check and try again
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+            </svg>
+        </p>
         <div>
             <label x-ref="emailLabel" for="email">Email</label>
-            <input x-on:focus="$refs.emailLabel.classList.toggle('text-red-500')" x-on:focusout="$refs.emailLabel.classList.remove('text-red-500')" class="block mt-1 w-full" type="email" name="email" required placeholder="Enter your email..." />
+            <input x-on:focus="$refs.emailLabel.classList.add('text-red-500')" x-on:focusout="emailInput ? '' : $refs.emailLabel.classList.remove('text-red-500')" class="block mt-1 w-full" type="email" name="email" required placeholder="Enter your email..." x-model="emailInput" />
         </div>
         <div class="mt-4">
             <label x-ref="passwordLabel" for="password">Password</label>
-            <input x-on:focus="$refs.passwordLabel.classList.add('text-red-500')" x-on:focusout="$refs.passwordLabel.classList.remove('text-red-500')" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Enter your password..." />
+            <input x-on:focus="$refs.passwordLabel.classList.add('text-red-500')" x-on:focusout="passwordInput ? '' : $refs.passwordLabel.classList.remove('text-red-500') " class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Enter your password..." x-model="passwordInput" />
         </div>
         <div class="block mt-4">
             <div class="inline-block">
@@ -29,6 +33,11 @@
         </div>
         <a class="text-sm inline-block mt-2" href="{{ route('password.request') }}">Register an Account</a>
     </form>
+   <div x-cloak x-show="submitSuccess">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 animate-bounce mx-auto" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+        </svg>
+   </div>
 </div>
 @endsection
 
@@ -39,6 +48,10 @@
     const loginForm = ({ csrfToken, loginURL }) => ({
         csrfToken,
         rememberMeClicked: false,
+        submitFailed: false,
+        submitSuccess: false,
+        emailInput: "",
+        passwordInput: "",
         init() {
             console.log(csrfToken);
         },
@@ -46,27 +59,24 @@
             const form = document.getElementById("f_login");
             const formData = new FormData(form);
 
-            const response = await fetch(loginURL, {
-                method: "post",
-                body: formData,
-                headers: {
-                    _token: csrfToken,
-                },
-            });
-            const json = await response.json();
+            try {
+                const response = await fetch(loginURL, {
+                    method: "post",
+                    body: formData,
+                });
+                const json = await response.json();
 
-            if (!json.success) {
-                this.onSubmitFail();
-                return;
+                if (!json.success) throw Error;
+
+                this.onSubmitSuccess();
+            } catch {
+                this.submitFailed = true;
+                this.submitSuccess = false;
             }
-
-            this.onSubmitSuccess();
         },
         onSubmitSuccess() {
-            console.log("Success");
-        },
-        onSubmitFail() {
-            console.log("Failed");
+            this.submitFailed = false;
+            this.submitSuccess = true;
         },
     });
 </script>
