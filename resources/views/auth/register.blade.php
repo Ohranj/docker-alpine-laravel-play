406b44b56c30372e7b2dd153ec4b87d47bd948ea
Ohranj <ajdorrington@hotmail.co.uk>
2022-04-12 21:29:07 +0100

    Added client side validation to initial register tab

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,58 +11,86 @@
             <span x-cloak x-text="progressBarText[stepsCompleted]"></span>
         </div>
     </div>
-    <div x-show="stepsCompleted == 0" class="mt-10 p-3 shadow-xl shadow-red-300 rounded">
-        <p class="mb-2">The information provided throughout the registration will go towards creating your profile card. This a publicly viewable snapshot of yourself.</p>
-        <small>You will have chance to see your card prior to confirming your registration.</small>
-        <form class="my-6 border-t-2">
-            <div class="my-4">
-                <label for="email">Email<sup>*</sup></label>
-                <input name="email" type="email" class="block mt-1 w-full" placeholder="Enter your email..." />
-            </div>
-            <div class="my-4">
-                <label for="password">Password<sup>*</sup></label>
-                <input name="password" type="password" class="block mt-1 w-full" placeholder="Enter your password..." />
-            </div>
-            <div class="my-4">
-                <label for="confirm-password">Confirm Password<sup>*</sup></label>
-                <input name="confirm-password" type="password" class="block mt-1 w-full" placeholder="Confirm your password..." />
-            </div>
-            <div class="my-4 flex gap-x-2">
-                <div class="flex-grow">
-                    <label for="firstname">Firstname<sup>*</sup></label>
-                    <input name="firstname" type="text" class="block mt-1 w-full" placeholder="Enter your firstname..." />
+    <div>
+        <p class="text-red-500 mt-5 text-sm text-center" x-text="errorText"></p>
+        <div x-show="stepsCompleted == 0" x-transition class="mt-5 p-3 shadow-xl shadow-red-300 rounded">
+            <p class="mb-2">The information provided throughout the registration will go towards creating your profile card. This a publicly viewable snapshot of yourself.</p>
+            <small>You will have chance to see your card prior to confirming your registration.</small>
+            <form class="my-6 border-t-2">
+                <div class="my-4">
+                    <label for="email">Email<sup>*</sup></label>
+                    <input name="email" type="email" class="block mt-1 w-full" placeholder="Enter your email..." x-model="inputData.email" />
                 </div>
-                <div class="flex-grow">
-                    <label for="firstname">Surname<sup>*</sup></label>
-                    <input name="firstname" type="text" class="block mt-1 w-full" placeholder="Enter your surname..." />
+                <div class="my-4">
+                    <label for="password">Password<sup>*</sup></label>
+                    <input name="password" type="password" class="block mt-1 w-full" placeholder="Enter your password..." x-model="inputData.password" />
                 </div>
+                <div class="my-4">
+                    <label for="confirm-password">Confirm Password<sup>*</sup></label>
+                    <input name="confirm-password" type="password" class="block mt-1 w-full" placeholder="Confirm your password..." x-model="inputData.confirmPassword" />
+                </div>
+                <div class="my-4 flex gap-x-2">
+                    <div class="flex-grow">
+                        <label for="firstname">Firstname<sup>*</sup></label>
+                        <input name="firstname" type="text" class="block mt-1 w-full" placeholder="Enter your firstname..." x-model="inputData.firstname" />
+                    </div>
+                    <div class="flex-grow">
+                        <label for="firstname">Surname<sup>*</sup></label>
+                        <input name="firstname" type="text" class="block mt-1 w-full" placeholder="Enter your surname..." x-model="inputData.surname" />
+                    </div>
+                </div>
+                <button class="app-btn app-btn-primary block mx-auto" @click.prevent="confirmFirstPressed">Confirm</button>
+            </form>
+        </div>
+        <div x-cloak x-show="stepsCompleted == 1" x-transition class="mt-5 p-3 shadow-xl shadow-red-300 rounded">
+            <div class="flex justify-center gap-x-4">
+                <button class="app-btn app-btn-secondary" @click.prevent="backPressed">Back</button>
+                <button class="app-btn app-btn-primary" @click.prevent="confirmSecondPressed">Confirm</button>
             </div>
-            <button class="app-btn app-btn-primary block mx-auto" @click.prevent="confirmPressed">Confirm</button>
-        </form>
-    </div>
-    <div x-show="stepsCompleted == 1" class="mt-10 p-3 shadow-xl shadow-red-300 rounded">
-        <div class="flex justify-center gap-x-4">
-            <button class="app-btn app-btn-secondary" @click.prevent="backPressed">Back</button>
-            <button class="app-btn app-btn-primary" @click.prevent="confirmPressed">Confirm</button>
         </div>
     </div>
 </div>
 @endsection
 
+<!-- prettier-ignore -->
+@section('script')
+<!-- prettier-ignore -->
 <script>
     const register = () => ({
         stepsCompleted: 0,
         progressBarText: ["Step 1", "Step 2", "Step 3"],
-        init() {
-            console.log("here");
+        errorTextArray: ['Please make sure all fields marked (*) are completed before proceeding', 'Please make sure the password and confirm password fields match.', 'Passwords should contain at least 8 digits and be made up of digits and uppercase / lowercase characters'],
+        errorText: null,
+        inputData: {
+            email: "",
+            password: "",
+            confirmPassword: "",
+            firstname: "",
+            surname: "",
         },
-        confirmPressed() {
-            if (this.stepsCompleted == 2) return;
+        confirmFirstPressed() {
+            try {
+                const allInputsFilled = Object.values(this.inputData).every((x) => x);
+                if (!allInputsFilled) throw Error(0);
+                const { password, confirmPassword } = this.inputData;
+                if (password !== confirmPassword) throw Error(1);
+                if (password.length < 8) throw Error(2);
+                if (!/\d/.test(password)) throw Error(2);
+                if (!/[A-Z]/.test(password)) throw Error(2);
+                if (!/[a-z]/.test(password)) throw Error(2);
+                this.stepsCompleted++
+                this.errorText = null
+            } catch (errCode) {
+                const val = errCode.message;
+                this.errorText = this.errorTextArray[val]
+            }
+        },
+        confirmSecondPressed() {
             this.stepsCompleted++;
         },
         backPressed() {
-            if (this.stepsCompleted == 0) return;
             this.stepsCompleted--;
         },
     });
 </script>
+@endsection
