bfc78fe65d63dbde85ecb0b02e3314aa99148407
Ohranj <ajdorrington@hotmail.co.uk>
2022-04-12 21:47:02 +0100

    Added syling for second page of register steps

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,6 +43,26 @@
             </form>
         </div>
         <div x-cloak x-show="stepsCompleted == 1" x-transition class="mt-5 p-3 shadow-xl shadow-red-300 rounded">
+            <p class="mb-2">Here you have the opportunity to share a little more about yourself. A tag line, a recent achievement or what you're looking forward to. Amongst some key tags you resonate with.</p>
+            <form class="my-6 border-t-2">
+                <div class="my-4">
+                    <label for="tagline">Tagline<sup>*</sup></label>
+                    <input name="tagline" type="text" class="block mt-1 w-full" placeholder="Enter a tagline..." maxlength="50" x-model="cardData.tagline" />
+                </div>
+                <div class="my-4">
+                    <label for="tags">Tags<sup>*</sup><small class="ml-4">Tags should be comma seperated.</small></label>
+                    <input name="tags" type="text" class="block mt-1 w-full" placeholder="Enter some tags... i.e. sleepy, home workout, in the zone" maxlength="50" x-model="cardData.tags" />
+                </div>
+                <div class="my-4">
+                    <label for="level">Experience Level<sup>*</sup></label>
+                    <select name="level" x-model="cardData.level" class="block mt-1 w-full text-black">
+                        <option>Master</option>
+                        <option>I know my way around</option>
+                        <option>Rookie</option>
+                        <option>Where am I?</option>
+                    </select>
+                </div>
+            </form>
             <div class="flex justify-center gap-x-4">
                 <button class="app-btn app-btn-secondary" @click.prevent="backPressed">Back</button>
                 <button class="app-btn app-btn-primary" @click.prevent="confirmSecondPressed">Confirm</button>
@@ -57,7 +77,7 @@
 <!-- prettier-ignore -->
 <script>
     const register = () => ({
-        stepsCompleted: 0,
+        stepsCompleted: 1,
         progressBarText: ["Step 1", "Step 2", "Step 3"],
         errorTextArray: ['Please make sure all fields marked (*) are completed before proceeding', 'Please make sure the password and confirm password fields match.', 'Passwords should contain at least 8 digits and be made up of digits and uppercase / lowercase characters'],
         errorText: null,
@@ -67,6 +87,11 @@
             confirmPassword: "",
             firstname: "",
             surname: "",
+        },
+        cardData: {
+            tagline: '',
+            tags: '',
+            level: ''
         },
         confirmFirstPressed() {
             try {
