b840cd7d886496f8f40cb5715f467c58c37c2330
Ohranj <ajdorrington@hotmail.co.uk>
2022-04-18 19:22:00 +0100

    Amended UI for verify-email view

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,44 +1,46 @@
-@extends('layouts.guest')
 <!-- prettier-ignore -->
+@extends('layouts.guest')
+
 @section('main-content')
-<div class="mb-4 text-sm text-gray-600">
-    {{
-        __(
-            "Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another."
-        )
-    }}
-</div>
-
-@if (session('status') == 'verification-link-sent')
-<div class="mb-4 font-medium text-sm text-green-600">
-    {{
-        __(
-            "A new verification link has been sent to the email address you provided during registration."
-        )
-    }}
-</div>
-@endif
-
-<div class="mt-4 flex items-center justify-between">
-    <form method="POST" action="{{ route('verification.send') }}">
-        @csrf
-
-        <div>
-            <button>
-                {{ __("Resend Verification Email") }}
+<div class="h-screen flex justify-center flex-col">
+    @if (session('status') == 'verification-link-sent')
+    <div class="text-center text-accent-blue flex flex-col gap-y-4">
+        A new verification link has been sent to the email address you provided
+        during registration. Please check your inbox.
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="app-btn app-btn-secondary">
+                Log Out
             </button>
-        </div>
-    </form>
+        </form>
+    </div>
+    @endif
 
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
+    <!-- prettier-ignore -->
+    @if (!session('status'))
+    <h1 class="text-center mb-4 text-xl">Please verify your email address</h1>
+    <div class="mb-4 text-sm">
+        You will need to verify your email address prior to navigating the site.
+        Please check your inbox or click the button below to resend a
+        verification link. A link will be sent to the email address you used
+        when registering for the site.
+    </div>
 
-        <button
-            type="submit"
-            class="underline text-sm text-gray-600 hover:text-gray-900"
-        >
-            {{ __("Log Out") }}
-        </button>
-    </form>
+    <div class="mt-4 flex gap-x-4 justify-center">
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="app-btn app-btn-secondary">
+                Log Out
+            </button>
+        </form>
+
+        <form method="POST" action="{{ route('verification.send') }}">
+            @csrf
+            <div>
+                <button class="app-btn app-btn-primary">Send Link</button>
+            </div>
+        </form>
+    </div>
+    @endif
 </div>
 @endsection
