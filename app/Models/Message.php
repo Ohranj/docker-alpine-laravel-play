c7ee3a9a385835aa41d183f425a4f7e536b5aca7
Ohranj <ajdorrington@hotmail.co.uk>
2022-04-23 12:25:58 +0100

    Create message table and model. Seeded new message data and applied encript to message model attribute get

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Message extends Model
 {
     use HasFactory;
+
+    public function setEncrypt($value) {
+        return $this->attributes['message'] = Crypt::encryptString($value);
+    }
 
     // protected $dispatchesEvents = [
     //     'saved' => UserSaved::class,
