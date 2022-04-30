6a4e6ca976a688fc23ce08aa016f452534387163
Ohranj <ajdorrington@hotmail.co.uk>
2022-04-30 18:26:49 +0100

    Added additional sections to diary modal form. Accordions for adding workout details, tags and a generic textarea

--- resources/views/components/diary-entry-modal.blade.php
+++ resources/views/components/diary-entry-modal.blade.php
@@ -11,7 +11,35 @@
                 </button>
             </div>
             <div class="p-6 text-slate-700">
-                <p class="text-left" x-text="humanDateString"></p>
+                <h4 class="text-right text-lg mb-4" x-text="humanDateString"></h4>
+                <template x-for="star of Object.keys(stars)">
+                    <svg xmlns="http://www.w3.org/2000/svg" @click="stars[star] = !stars[star]" class="h-8 w-8 inline-block cursor-pointer" :fill="stars[star] ? '#facc15' : 'none'" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+                    </svg>
+                </template>
+                <div class="border rounded mt-4">
+                    <h5 @click="showAddWorkout = !showAddWorkout" class="border cursor-pointer py-2 text-lg flex-grow relative">
+                        Add Workout Details
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 absolute top-3 right-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path x-show="showAddWorkout" fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+                            <path x-cloak x-show="!showAddWorkout" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </h5>
+                    <div x-collapse x-show="showAddWorkout" class="border">
+                        Workout details
+                    </div>
+                    <h5 @click="showAddTags = !showAddTags" class="border cursor-pointer py-2 text-lg flex-grow relative">
+                        Add Tags
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 absolute top-3 right-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path x-show="showAddTags" fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+                            <path x-cloak x-show="!showAddTags" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </h5>
+                    <div x-collapse x-show="showAddTags" class="border">
+                        Noteables
+                    </div>
+                </div>
+                <textarea name="textEntry" rows="5" class="w-full rounded mt-2" placeholder="Here is a space to write some additional information for the day..." ></textarea>
             </div>
             <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 bg-slate-700">
                 <button @click="showModal = false" type="button" class="app-btn app-btn-secondary ml-auto">Close</button>
